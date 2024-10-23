d4a6ffee126f91b7bfaf7ae630df0aa4c8cc2bba
jurko <jurko.bossetroll@gmail.com>
2024-10-23 23:25:47 +0200

    Change file admissible size constants

--- src/ransom_note/config.clj
+++ src/ransom_note/config.clj
@@ -3,6 +3,6 @@
 
 (def CHUNK_SIZE 1024) ;; Define the chunk size constant
 
-(def SMALL_MAGAZINE_FILE_SIZE_MB (* 4 1024 1024))  ;; 4 MB in bytes
+;;(def SMALL_MAGAZINE_FILE_SIZE_MB (* 4 1024 1024))  ;; 4 MB in bytes
 
-(def MAX_MESSAGE_FILE_SIZE_MB (* 4 1024 1024))  ;; 4 MB in bytes
+(def MAX_MESSAGE_FILE_SIZE_MB (* 1024))  ;; 1 KB in bytes
