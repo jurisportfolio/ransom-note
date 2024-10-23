1c23da7b2ef8ee0645b3856497a22ce416451dc3
jurko <jurko.bossetroll@gmail.com>
2024-10-23 23:31:36 +0200

    Add comments to can-make-ransom-note function

--- src/ransom_note/core.clj
+++ src/ransom_note/core.clj
@@ -16,15 +16,25 @@
   - magazine-file-name: A string representing the path to the magazine file."
 
   [message-file-name magazine-file-name]
+  ;; Count the non-whitespace characters in the message
+  ;; This creates a map where keys are characters and values are their counts
   (let [message-count (utils/count-non-whitespace-chars (utils/read-message message-file-name))
+        ;; Create a buffer to read chunks of characters from the magazine file
         buffer (char-array config/CHUNK_SIZE)]
     ;;(println {:message message-count})
+    ;; Open the magazine file for reading
     (with-open [reader (io/reader magazine-file-name)]
+      ;; Initialize an accumulator to store character counts from the magazine
+      ;; and an atom to track the number of bytes read in each iteration
       (loop [acc {}
              bytes-read-atom (atom 0)]
+        ;; Read a chunk of data from the magazine file into the buffer
         (when (utils/read-from-file bytes-read-atom reader buffer)
+          ;; Convert the buffer into a string for processing
           (let [chunk (String. buffer 0 @bytes-read-atom)
+                ;; Count the non-whitespace characters in the chunk
                 chunk-count (utils/count-non-whitespace-chars chunk)
+                ;; Update the accumulated character counts from the magazine
                 updated-acc (merge-with + acc chunk-count)]
             ;; Check if accumulated counts are enough to construct the message
             ;;(println {:magazine updated-acc})
