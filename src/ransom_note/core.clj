c8fcdb2a149739c21bbff4aa38d7e933e2e66734
jurko <jurko.bossetroll@gmail.com>
2024-10-23 20:01:58 +0200

    Change core: refactore - add enough-letters func

--- src/ransom_note/core.clj
+++ src/ransom_note/core.clj
@@ -4,11 +4,18 @@
             [ransom-note.config :as config]))
 
 
+(defn enough-letters
+  [message-count magazine-count]
+  (every? (fn [[letter count]]
+            (>= (get magazine-count letter 0) count))
+          message-count))
+
+
 (defn can-make-ransom-note
   [message-file-name magazine-file-name]
   (println "Start function: ")
   (let [message-count (utils/count-non-whitespace-chars (utils/read-message message-file-name))]
-    (println {:message message-count})
+    ;;(println {:message message-count})
     (with-open [reader (io/reader magazine-file-name)]
       (loop [acc {}
              bytes-read (atom 0)]
@@ -18,10 +25,8 @@
                   chunk-count (utils/count-non-whitespace-chars chunk)
                   updated-acc (merge-with + acc chunk-count)]
               ;; Check if accumulated counts are enough to construct the message
-              (println {:magazine updated-acc})
-              (if (every? (fn [[letter count]]
-                            (>= (get updated-acc letter 0) count))
-                          message-count)
+              ;;(println {:magazine updated-acc})
+              (if (enough-letters message-count updated-acc)
                 true ;; Stop processing and return true
                 (recur updated-acc bytes-read)))))))))
 
