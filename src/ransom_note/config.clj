e036062acb65deb4bf2449bc0d202b9c378aa5d0
jurko <jurko.bossetroll@gmail.com>
2024-10-24 15:36:06 +0200

    Add more functional tests

--- src/ransom_note/config.clj
+++ src/ransom_note/config.clj
@@ -1,7 +1,7 @@
 (ns ransom-note.config)
 
 
-(def CHUNK_SIZE 1024) ;; Define the chunk size constant
+(def CHUNK_SIZE (* 1024)) ;; Define the chunk size constant 1KB
 
 ;;(def SMALL_MAGAZINE_FILE_SIZE_MB (* 4 1024 1024))  ;; 4 MB in bytes
 
